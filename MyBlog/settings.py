aa088b1faec762242f76bd489f3a3f97c191be48
Bing <as505160630@163.com>
2018-04-06 17:25:56 +0800

    修改settings

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9_n=3gdccdqwz=ec&u8+2)x7m2+8al)r_*9x(4jhwrm_69&8j6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cloudbing.pythonanywhere.com','*']
 
 
 # Application definition
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
